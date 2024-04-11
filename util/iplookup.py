e723682404554d57b94edbcef19fd41bae4c6220
Aqua <151952016+AquaLT@users.noreply.github.com>
2024-04-11 14:07:41 -0700

    Update iplookup.py

--- util/iplookup.py
+++ util/iplookup.py
@@ -1,5 +1,8 @@
+import random
 import requests
+
 from colorama import Fore
+
 w = Fore.WHITE
 r = Fore.RED
 b = Fore.LIGHTBLACK_EX
@@ -12,27 +15,42 @@ lb = Fore.LIGHTBLUE_EX
 fr = Fore.RESET
 ly = Fore.LIGHTYELLOW_EX
 
-def phone_locator(phone_number, api_key):
-    url = f"https://api.apilayer.com/number_verification/validate?number={phone_number}"
+
+
+ip_input = input(f"{r}IP : {b}")
+
+
+def lookup():
+    
     headers = {
-        "apikey": api_key
-    }
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    return data
+        'authority': 'ipinfo.io',
+        'accept': '*/*',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'content-type': 'application/json',
+        'referer': 'https://ipinfo.io/',
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        }
+    r = requests.get(f'https://ipinfo.io/widget/demo/{ip_input}', headers=headers)
+    ip = r.json()['data']['ip']
+    city = r.json()['data']['city']
+    region = r.json()['data']['region']
+    country = r.json()['data']['country']
+    timezone = r.json()['data']['timezone']
+    address = r.json()['data']['abuse']['address']
+    country = r.json()['data']['abuse']['country']
+    print(f'{Fore.LIGHTRED_EX}IP: {Fore.LIGHTBLACK_EX}{ip}')
+    print(f'{Fore.LIGHTRED_EX}City: {Fore.LIGHTBLACK_EX}{city}')
+    print(f'{Fore.LIGHTRED_EX}Region: {Fore.LIGHTBLACK_EX}{region}')
+    print(f'{Fore.LIGHTRED_EX}Country: {Fore.LIGHTBLACK_EX}{country}')
+    print(f'{Fore.LIGHTRED_EX}Timezone: {Fore.LIGHTBLACK_EX}{timezone}')
+    print(f'{Fore.LIGHTRED_EX}Address: {Fore.LIGHTBLACK_EX}{address}')
 
-def print_phone_details(phone_details):
-    print(f"{lr}Phone Details: {b}")
-    print(f"{lr}Number: {b}{phone_details['number']}")
-    print(f"{lr}Country: {b}{phone_details['country_name']} ({phone_details['country_code']})")
-    print(f"{lr}Carrier: {b}{phone_details['carrier']}")
-    print(f"{lr}Line Type: {b}{phone_details['line_type']}")
-    print(f"{lr}Location: {b}{phone_details['location']}")
-    print(f"{lr}Valid: {b}{phone_details['valid']}")
 
-api_key = "22WfDqB635jMOWkxdeC1r2BXew1tXHLU"
-
-phone_number = input(Fore.RED + f"IP : {b}")
-
-phone_details = phone_locator(phone_number, api_key)
-print_phone_details(phone_details)
+lookup()
+input()
